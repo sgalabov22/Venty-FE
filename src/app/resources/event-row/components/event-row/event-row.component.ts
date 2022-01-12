5d3c9617fcd1a208655598208710803d1a4406df
Stefan Galabov <galabovs@aforbes.com>
2022-01-12 03:24:58 +0200

    feature(calendar-layout): stuff on the calendar

---
+++ src/app/resources/event-row/components/event-row/event-row.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-event-row',
+  templateUrl: './event-row.component.html',
+  styleUrls: ['./event-row.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class EventRowComponent {
+  @Input() elementWidth: string;
+  @Input() title: string;
+  @Input() color: string;
+
+}
---
+++ src/app/resources/event-row/components/index.ts
@@ -0,0 +1,1 @@
+export * from './event-row/event-row.component';
