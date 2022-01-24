081e6d2655dfe28a24eb8a41637448ece781626d
Stefan Galabov <galabovs@aforbes.com>
2022-01-24 02:33:26 +0200

    feat: created map searches and elements and nearby options

---
+++ src/app/maps/components/location-component/location-component.component.ts
@@ -0,0 +1,32 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Input,
+  OnInit
+} from '@angular/core';
+
+@Component({
+  selector: 'app-location-component',
+  templateUrl: './location-component.component.html',
+  styleUrls: ['./location-component.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LocationComponentComponent {
+  @Input() locationElement: google.maps.places.PlaceResult;
+  public locationPhoto: string;
+
+  constructor() {
+    console.log(this.locationElement);
+
+    if (
+      this.locationElement?.photos &&
+      this.locationElement.photos.length > 0
+    ) {
+      this.locationPhoto = this.locationElement.photos[0].getUrl({
+        maxWidth: 150,
+        maxHeight: 150
+      });
+    }
+  }
+
+}
